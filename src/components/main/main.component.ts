c7e5f7dd36d136949b953e2fb496e02a80a4852c
Nijat <gurbanli.nidjat001@gmail.com>
2025-03-17 01:43:48 +0400

    added pagination in the main page

--- src/components/main/main.component.ts
+++ src/components/main/main.component.ts
@@ -4,11 +4,11 @@ import {HeaderComponent} from '../header/header.component';
 import {ManipulateDataService} from '../../services/manipulate-data.service';
 import {Item} from '../../models/Item';
 import {FormsModule} from '@angular/forms';
-import {NgStyle} from '@angular/common';
+import {NgForOf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [HttpClientModule, HeaderComponent, FormsModule, NgStyle],
+  imports: [HttpClientModule, HeaderComponent, FormsModule, NgStyle, NgForOf],
   providers: [ManipulateDataService],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
@@ -19,13 +19,40 @@ export class MainComponent implements OnInit {
   public items: Item[] = [];
   public filteredItems: Item[] = [];
   public flag: boolean = true;
-  category: string = "";
+  public category: string = "";
+  // Pagination variables
+  public currentPage: number = 1;
+  public pageSize: number = 2;
+  public totalPages: number = 0;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.items = this.myService.getItems();
+    this.totalPages = Math.ceil(this.items.length / this.pageSize);
+  }
+
+  // Pagination methods
+  public getCurrentPageItems(): Item[] {
+    const filteredCategories = this.filterItemsByCategory();
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = Math.min(startIndex + this.pageSize, filteredCategories.length);
+    return filteredCategories.slice(startIndex, endIndex);
+  }
+
+  public setPage(pageNumber: number): void {
+    if (pageNumber >= 1 && pageNumber <= this.totalPages) {
+      this.currentPage = pageNumber;
+    }
+  }
+
+  public getPageNumbers(): number[] {
+    let pages: number[] = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
   }
 
   public filterItemsByCategory(): Item[] {
